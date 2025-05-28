28afc934d288097333d56c25615c86462986dc78
David Turchak <david.turchak@gmail.com>
2025-05-28 13:02:34 +0300

    Added help to connection script

--- connect-iscsi-sessions.ps1
+++ connect-iscsi-sessions.ps1
@@ -1,27 +1,63 @@
+<#
+.SYNOPSIS
+    Connects the local iSCSI initiator to one or more iSCSI target portals.
+
+.DESCRIPTION
+    This script configures iSCSI target portals and establishes sessions from the local initiator 
+    to multiple target portal IP addresses, optionally with multipath and session persistence. 
+    It generates target addresses by incrementing the last octet of a given base IP.
+
+.PARAMETER FirstTargetPortalAddress
+    The base IP address of the first iSCSI target portal. 
+    Example: 10.209.88.4
+
+.PARAMETER Number_of_cnodes
+    The number of iSCSI target nodes (CNODES) to connect to. 
+    Each node IP is calculated by incrementing the last octet. Default is 2.
+
+.PARAMETER NumOfSessionsPerTargetPortalAddress
+    The number of iSCSI sessions to create per target portal address. Default is 1.
+
+.EXAMPLE
+    .\Connect-iSCSITargets.ps1 -FirstTargetPortalAddress "10.209.88.4"
+
+.EXAMPLE
+    .\Connect-iSCSITargets.ps1 -FirstTargetPortalAddress "10.209.88.4" -Number_of_cnodes 4 -NumOfSessionsPerTargetPortalAddress 2
+
+.NOTES
+    Author: David Turchak
+    Date: 2025-05-28
+#>
+
+[CmdletBinding()]
 param (
+    [Parameter(Mandatory = $true, HelpMessage = "The base IP address of the first iSCSI target portal. Example: 10.209.88.4")]
     [string]$FirstTargetPortalAddress,
+
+    [Parameter(HelpMessage = "The number of iSCSI target nodes (CNODES) to connect to. Default is 2.")]
     [int]$Number_of_cnodes = 2,
+
+    [Parameter(HelpMessage = "The number of iSCSI sessions per target portal address. Default is 1.")]
     [int]$NumOfSessionsPerTargetPortalAddress = 1
 )
 
 function Get-LocaliSCSIAddress {
-    # Get all IPv4 addresses of network interfaces excluding loopback
+    <#
+    .SYNOPSIS
+        Retrieves the local IPv4 address used for iSCSI communication.
+
+    .DESCRIPTION
+        Excludes the loopback interface and returns the IPv4 address 
+        with the highest last octet, assuming it is the primary interface.
+    #>
+
     $networkInterfaces = Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ne 'Loopback Pseudo-Interface 1' }
-
-    # Sort network interfaces by the last octet of their IPv4 addresses
     $sortedInterfaces = $networkInterfaces | Sort-Object { [int]$_.IPAddress.Split('.')[-1] } -Descending
-
-    # Select the first interface from the sorted list
-    $LocaliSCSIAddress = $sortedInterfaces[0].IPAddress
-
-    return $LocaliSCSIAddress
+    return $sortedInterfaces[0].IPAddress
 }
 
-
-# Get the local iSCSI address
 $LocaliSCSIAddress = Get-LocaliSCSIAddress
 
-# Increment the last octet of the first TargetPortalAddress to create an array of IP addresses
 $TargetPortalAddresses = @()
 $baseIP = $FirstTargetPortalAddress.Substring(0, $FirstTargetPortalAddress.LastIndexOf("."))
 $lastOctet = [int]$FirstTargetPortalAddress.Substring($FirstTargetPortalAddress.LastIndexOf(".") + 1)
@@ -29,17 +65,19 @@ for ($i = 0; $i -lt $Number_of_cnodes; $i++) {
     $TargetPortalAddresses += "$baseIP.$($lastOctet + $i)"
 }
 
-# Configures an iSCSI target portal.
 foreach ($TargetPortalAddress in $TargetPortalAddresses) {
-   Write-Host "Adding target portal for: $TargetPortalAddress"
-   New-IscsiTargetPortal -TargetPortalAddress $TargetPortalAddress -InitiatorPortalAddress $LocaliSCSIAddress 
+    Write-Host "Adding target portal for: $TargetPortalAddress"
+    New-IscsiTargetPortal -TargetPortalAddress $TargetPortalAddress -InitiatorPortalAddress $LocaliSCSIAddress 
 }
 
 $NodeAddres = Get-IscsiTarget
 
-# Establishes a connection between the local iSCSI initiator and an iSCSI target device.
 1..$NumOfSessionsPerTargetPortalAddress | ForEach-Object {
     foreach ($TargetPortalAddress in $TargetPortalAddresses) {
-        Connect-IscsiTarget -IsMultipathEnabled $true -TargetPortalAddress $TargetPortalAddress -InitiatorPortalAddress $LocaliSCSIAddress -IsPersistent $true -NodeAddress $NodeAddres.NodeAddress
+        Connect-IscsiTarget -IsMultipathEnabled $true `
+                            -TargetPortalAddress $TargetPortalAddress `
+                            -InitiatorPortalAddress $LocaliSCSIAddress `
+                            -IsPersistent $true `
+                            -NodeAddress $NodeAddres.NodeAddress
     }
 }
