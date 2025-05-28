25a7183986b9f4da62f14a85526ca04a6366db60
David Turchak <david.turchak@gmail.com>
2025-05-28 11:47:39 +0300

    Added MS iscsi initiator handeling in disconnection script

--- disconnect-iscsi-host.ps1
+++ disconnect-iscsi-host.ps1
@@ -12,10 +12,27 @@ start-sleep 1
 # Get the list of iSCSI target portals
 $targetPortals = Get-IscsiTargetPortal
 
+function Get-LocaliSCSIAddress {
+    # Get all IPv4 addresses of network interfaces excluding loopback
+    $networkInterfaces = Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ne 'Loopback Pseudo-Interface 1' }
+
+    # Sort network interfaces by the last octet of their IPv4 addresses
+    $sortedInterfaces = $networkInterfaces | Sort-Object { [int]$_.IPAddress.Split('.')[-1] } -Descending
+
+    # Select the first interface from the sorted list
+    $LocaliSCSIAddress = $sortedInterfaces[0].IPAddress
+
+    return $LocaliSCSIAddress
+}
+
+
+# Get the local iSCSI address
+$LocaliSCSIAddress = Get-LocaliSCSIAddress
+
 # Loop through each target portal and remove it
 foreach ($portal in $targetPortals) {
     try {
-        Remove-IscsiTargetPortal -TargetPortalAddress $portal.TargetPortalAddress -confirm:$false -ErrorAction Stop
+        Remove-IscsiTargetPortal -TargetPortalAddress $portal.TargetPortalAddress -InitiatorPortalAddress $LocaliSCSIAddress -confirm:$false -ErrorAction Stop
         Write-Host "Removed target portal: $($portal.TargetPortalAddress)"
     } catch {
         Write-Host "Error removing target portal $($portal.TargetPortalAddress): $_"
